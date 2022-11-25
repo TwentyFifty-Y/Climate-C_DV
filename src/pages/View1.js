2fd4c50f1c4dded54d055ce7f807a1d2954b03c9
PortalFan202 <anibaldonoso1@gmail.com>
2022-11-25 21:24:41 +0200

    Removed non-used views

--- src/pages/View1.js
+++ src/pages/View1.js
@@ -8,16 +8,7 @@ export default function View1() {
       <h1 className="title">Atmospheric CO<span id="sub">2</span> and Emissions</h1>
       <div id="flex-container">
         <div className="flex-item">
-          <span>{v1()}</span>
-        </div> 
-        <div className="flex-item">
           <span>{v2()}</span>
-        </div>
-        <div className="flex-item">
-          <span>{v2()}</span>
-        </div> 
-        <div className="flex-item">
-          <span>{v1()}</span>
         </div>
       </div>
     </div>
