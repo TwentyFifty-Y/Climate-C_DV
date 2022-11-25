f97aac28094751b6bf5ef81d73677b3e7f26e1f7
PortalFan202 <anibaldonoso1@gmail.com>
2022-11-25 21:23:48 +0200

    Changed graph time unit from month to year

--- src/pages/visualisations/v2.js
+++ src/pages/visualisations/v2.js
@@ -189,7 +189,7 @@ export default function V2() {
             xAxis: {
                 type: "time",
                 time: {
-                    unit: "month",
+                    unit: "year",
                 },
             },
             yAxis: {
