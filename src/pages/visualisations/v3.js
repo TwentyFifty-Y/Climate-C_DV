e7b08122b82dd65ddff273255139fbc3e7886077
PortalFan202 <anibaldonoso1@gmail.com>
2022-11-25 21:24:56 +0200

    Created blank file for View 3

---
+++ src/pages/visualisations/v3.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { useState, useEffect } from "react"
+import { Chart } from "chart.js/auto";
+import { Line } from 'react-chartjs-2';
+import axios from 'axios';
+import 'chartjs-adapter-luxon';
+
+export default function View3() {
+    return
+    <div></div>
+}
