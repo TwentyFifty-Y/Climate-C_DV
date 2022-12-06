9d887a0371ca4c3511b709c01eb4c285c8d18d2b
PortalFan202 <anibaldonoso1@gmail.com>
2022-12-06 20:18:00 +0200

    V9 seemingly finished

--- src/pages/visualisations/v9.js
+++ src/pages/visualisations/v9.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import { Chart } from "chart.js/auto";
 import { Line, Doughnut } from 'react-chartjs-2';
 import axios from 'axios';
@@ -11,19 +11,11 @@ export default function V9() {
     const [sectorData, setSectorData] = useState([]);
     const [subSectorData, setSubSectorData] = useState([]);
     const [infraSubSectorData, setInfraSubSectorData] = useState([]);
+
+    const chartRef = useRef(null);
+
     const LINK = "//localhost:3000";
 
-
-    function view9SectorHandler(array) {
-        let data = array.sector.map((item) => {
-            return {
-                sector: item.sector,
-                percent: item.percent,
-                id: item.id
-            }
-        });
-        return data;
-    }
     const Text = () => {
         const [showText, setShowText] = useState(false);
 
@@ -44,7 +36,6 @@ export default function V9() {
         )
     };
 
-
     useEffect(() => {
         axios.get(LINK + "/views?id=view9Main").then((response) => {
             setSectorData(response.data.sector);
@@ -55,7 +46,6 @@ export default function V9() {
 
 
     const chartData = (dataArray) => {
-
         return {
             labels: dataArray.map((item) => {
                 return item.sector;
@@ -65,7 +55,7 @@ export default function V9() {
                 data: dataArray.map((item) => {
                     return item.percent;
                 }),
-                ids: dataArray.map((item) =>{
+                ids: dataArray.map((item) => {
                     return item.id
                 }),
                 backgroundColor: randomColor({
@@ -76,6 +66,9 @@ export default function V9() {
         }
 
     }
+
+    let sync = 0;
+    let id;
     const options = {
         //add a click event to the chart
         onClick: (event, elements) => {
@@ -83,26 +76,82 @@ export default function V9() {
             if (elements.length > 0) {
                 //get the index of the bar
                 const index = elements[0].index;
-                //get the label of the bar
-                const label = chartData(sectorData).labels[index];
-                //get the value of the bar
-                const value = chartData(sectorData).datasets[0].data[index];
-                //get the id of the bar
-                const id = sectorData[index].id;
-                console.log(subSectorData)
-                console.log(infraSubSectorData)
-                console.log(chartData(sectorData))
-                var s = "energy"
-                console.log(subSectorData[s])
+                console.log(elements[0].index)
+                //get the chart
+                const chart = chartRef.current;
+
+                function chartReset(chart) {
+                    //set the labels
+                    chart.config._config.data.labels = sectorData.map((item) => {
+                        return item.sector;
+                    })
+                    //set the data
+                    chart.config._config.data.datasets[0].data = sectorData.map((item) => {
+                        return item.percent;
+                    })
+                }
+
+                function chartUpdate(chart, data, id) {
+                    //change the labels
+                    chart.config._config.data.labels = data[id].map((item) => {
+                        return item.subSector;
+                    })
+                    //change the data
+                    chart.config._config.data.datasets[0].data = data[id].map((item) => {
+                        return item.percent;
+                    })
+                    //change the ids
+                    chart.config._config.data.datasets[0].ids = data[id].map((item) => {
+                        return item.id;
+                    })
+                }
+
+                switch (sync) {
+                    case 0:
+                        id = sectorData[index].id
+                        console.log(id)
+                        chartUpdate(chart, subSectorData, id);
+                        chart.update();
+                        sync = 1;
+                        break;
+                    case 1:
+                        console.log(subSectorData)
+                        console.log(id)
+                        console.log(index)
+
+                        if (id == "energy") {
+                            id = subSectorData[id][index].id
+
+                            if (id) {
+                                chartUpdate(chart, infraSubSectorData, id);
+                                chart.update();
+                                sync = 2;
+                            } else {
+                                sync = 1;
+                                id = "energy"
+                            }
+                        } else {
+                            chartReset(chart);
+                            chart.update();
+                            sync = 0;
+                        }
+                        break;
+                    case 2:
+                        chartReset(chart);
+                        chart.update();
+                        sync = 0;
+                    default:
+                        break;
+                }
                 //show an alert with the label and value
-                alert(`The sector ${label} has ${value}% of the global emissions`);
+                //alert(`The sector ${label} has ${value}% of the global emissions`);
             }
         }
     };
 
     return (
         <div /*style={{ width: "1000px" }}*/ className="view-canvas doughnut-chart">
-            <Doughnut data={chartData(sectorData)} options={options} />
+            <Doughnut ref={chartRef} data={chartData(sectorData)} options={options} />
             <Text />
         </div>
     );
