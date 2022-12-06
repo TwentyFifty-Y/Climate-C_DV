8802bd41b2794494242805ec90fbea614f70b801
PortalFan202 <anibaldonoso1@gmail.com>
2022-12-06 15:30:33 +0200

    Added test click function

--- src/pages/visualisations/v9.js
+++ src/pages/visualisations/v9.js
@@ -61,20 +61,32 @@ export default function V9() {
                     return item.percent;
                 }),
                 backgroundColor: randomColor({
-                    count: v9Data.length,
-                    luminosity: 'light',
-                    hue: 'random'
+                    count: 4,
+                    hue: 'red'
                 }),
             }]
         }
 
     }
     const options = {
-
+        //add a click event to the chart
+        onClick: (event, elements) => {
+            //if the click is on a bar
+            if (elements.length > 0) {
+                //get the index of the bar
+                const index = elements[0].index;
+                //get the label of the bar
+                const label = chartData(v9Data).labels[index];
+                //get the value of the bar
+                const value = chartData(v9Data).datasets[0].data[index];
+                //show an alert with the label and value
+                console.log(chartData(v9Data))
+            }
+        }
     };
 
     return (
-        <div /*style={{ width: "1000px" }}*/ className="view-canvas">
+        <div /*style={{ width: "1000px" }}*/ className="view-canvas doughnut-chart">
             <Doughnut data={chartData(v9Data)} options={options} />
             <Text />
         </div>
